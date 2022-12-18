d2655d0cbc5f9dc8a40485696ddffac658e621da
Franco Moya <franco.8moya@gmail.com>
2022-12-18 00:06:07 -0300

    efecto en el menú

--- menu.js
+++ menu.js
@@ -4,7 +4,7 @@ let menu = document.getElementById("menu");
 btn.addEventListener("click", function () {
     
     if (menu.style.left == "-15px") {
-        menu.style.left = "-250px"
+        menu.style.left = "-300px"
     } else {
         menu.style.left = "-15px";
     }
